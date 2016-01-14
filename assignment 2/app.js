12f5d84ef68119eab98ce47f0579d1af94f58357
Tom Siechert <siechert@hotmail.com>
2016-01-13 20:37:07 -0800

    day 2 files

---
+++ assignment 2/app.js
@@ -0,0 +1,12 @@
+//ask the user for first and last name
+var userFirstname = prompt("What is your first name?");
+var userLastname = prompt("What is your last name?");
+console.log(userFirstname);
+
+//alert your last name
+alert(userLastname);
+
+//confirm (anything); alert the result of the confirm
+
+var userLottery = confirm("Did you just win tonight's PowerBall lottery?");
+alert(userLottery);
---
+++ myjsfile.js
@@ -0,0 +1,13 @@
+//tell the user something
+
+alert("Hello, I'm an alert. I'll just wait here until you close me.");
+
+//confirm a user’s action
+
+var userChoice = confirm("Are you sure you want to do that?");
+console.log(userChoice);
+
+//ask the user for input
+
+var username = prompt("What is your name?");
+console.log(username)
